8d031e83b864b16aa53d293877768ef684d015d3
libstudent <libstudent@union.edu>
2024-10-02 15:05:37 -0400

    changed the location of input and output files

--- app/src/config.py
+++ app/src/config.py
@@ -1,12 +1,13 @@
 import os
 
 ROOT = os.path.dirname(__file__)
+MAINROOT = os.path.dirname(ROOT)
 BATCH_SIZE = 10
-APP_ROOT = os.path.join(ROOT, '..')
-DEFAULT_INPUT_FILE = os.path.join(APP_ROOT, 'results.xlsx')
-DEFAULT_OUTPUT_FILE = os.path.join(APP_ROOT, 'results_triaged.xlsx')
+APP_ROOT = os.path.join(ROOT)
+DEFAULT_INPUT_FILE = os.path.join(MAINROOT, 'inputs\\results.xlsx')
+DEFAULT_OUTPUT_FILE = os.path.join(MAINROOT, 'outputs\\results_triaged.xlsx')
 ACCEPTED_EXTENSIONS = ('csv', 'xlsx', 'xls')
 DEFAULT_EXT = 'xlsx'
 DEFAULT_DEBUG_FN = 'debug'
-DEFAULT_LOG_DIR = os.path.join(APP_ROOT, 'logs')
+DEFAULT_LOG_DIR = os.path.join(MAINROOT, 'logs')
 DEFAULT_DEBUG_FP = os.path.join(DEFAULT_LOG_DIR, DEFAULT_DEBUG_FN)
