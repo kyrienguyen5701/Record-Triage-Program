e267e985ee98349c5c8f97e0d5eaa36f385def89
James Gaskell <james.gaskell@Jamess-MacBook-Pro-5.local>
2024-10-04 15:43:00 -0400

    made changes to allow the app to run on mac

--- app/src/config.py
+++ app/src/config.py
@@ -1,11 +1,20 @@
 import os
+import platform
 
 ROOT = os.path.dirname(__file__)
 MAINROOT = os.path.dirname(ROOT)
 BATCH_SIZE = 10
 APP_ROOT = os.path.join(ROOT)
-DEFAULT_INPUT_FILE = os.path.join(MAINROOT, 'inputs\\results.xlsx')
-DEFAULT_OUTPUT_FILE = os.path.join(MAINROOT, 'outputs\\results_triaged.xlsx')
+
+# Accounting fot the differences in file structure between Mac and Windows
+plat = platform.platform() 
+if "macOS" in plat:
+    DEFAULT_INPUT_FILE = os.path.join(MAINROOT, 'inputs/results.xlsx')
+    DEFAULT_OUTPUT_FILE = os.path.join(MAINROOT, 'outputs/results_triaged.xlsx')
+else:
+    DEFAULT_INPUT_FILE = os.path.join(MAINROOT, 'inputs\\results.xlsx')
+    DEFAULT_OUTPUT_FILE = os.path.join(MAINROOT, 'outputs\\results_triaged.xlsx')
+    
 ACCEPTED_EXTENSIONS = ('csv', 'xlsx', 'xls')
 DEFAULT_EXT = 'xlsx'
 DEFAULT_DEBUG_FN = 'debug'
