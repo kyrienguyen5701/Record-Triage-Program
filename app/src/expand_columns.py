0ca6cb4fd6fb65e2f42327347a089052d26728bb
libstudent <libstudent@union.edu>
2024-10-16 14:30:29 -0400

    updated to reflect changes to output file

--- app/src/expand_columns.py
+++ app/src/expand_columns.py
@@ -7,21 +7,21 @@ def expand_columns(filepath):
     worksheet = wb.active
 
     column_widths = {
-        'A' : 22,
-        'B' : 14,
-        'C' : 27,
-        'D' : 32,
-        'E' : 32,
-        'F' : 27,
-        'G' : 37,
-        'H' : 12,
-        'I' : 40,
-        'J' : 20,
-        'K' : 23,
-        'L' : 23,
-        'M' : 23,
-        'N' : 23,
-        'O' : 23}
+        'A' : 22, # MMS ID
+        'B' : 15, # OCLC Number
+        'C' : 27, # Floor Status
+        'D' : 32, # Size Status
+        'E' : 32, # Format Assessment
+        'F' : 35, # Call Number
+        'G' : 37, # Title
+        'H' : 14, # Brief Level
+        'I' : 40, # Overall Condition
+        'J' : 23, # Illustration Status
+        'K' : 25, # Bibliography Status
+        'L' : 23, # Index Status
+        'M' : 17, # Pub Location
+        'N' : 23 # Coding Problems
+        }
 
     for column in column_widths.keys():
         worksheet.column_dimensions[column].width = column_widths[column]
