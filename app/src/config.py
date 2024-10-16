29b5e369ae9a345aad162ed8d0a97582bff9b769
libstudent <libstudent@union.edu>
2024-10-16 14:30:00 -0400

    changed OS detection

--- app/src/config.py
+++ app/src/config.py
@@ -5,15 +5,25 @@ ROOT = os.path.dirname(__file__)
 MAINROOT = os.path.dirname(ROOT)
 BATCH_SIZE = 10
 APP_ROOT = os.path.join(ROOT)
+PLATFORM = ""
+
+input_filename = 'biblio_data.xlsx'
+output_filename = 'results_triaged.xlsx'
 
 # Accounting fot the differences in file structure between Mac and Windows
 plat = platform.platform() 
+
 if "macOS" in plat:
-    DEFAULT_INPUT_FILE = os.path.join(MAINROOT, 'inputs/results.xlsx')
-    DEFAULT_OUTPUT_FILE = os.path.join(MAINROOT, 'outputs/results_triaged.xlsx')
+    PLATFORM = 'macOS'
+    DEFAULT_INPUT_FILE = os.path.join(MAINROOT, 'inputs/'+ input_filename)
+    DEFAULT_OUTPUT_FILE = os.path.join(MAINROOT, 'outputs/'+ output_filename)
+elif 'Windows' in plat:
+    PLATFORM = 'windows'
+    DEFAULT_INPUT_FILE = os.path.join(MAINROOT, 'inputs\\'+ input_filename)
+    DEFAULT_OUTPUT_FILE = os.path.join(MAINROOT, 'outputs\\'+ output_filename)
 else:
-    DEFAULT_INPUT_FILE = os.path.join(MAINROOT, 'inputs\\results.xlsx')
-    DEFAULT_OUTPUT_FILE = os.path.join(MAINROOT, 'outputs\\results_triaged.xlsx')
+    DEFAULT_INPUT_FILE = os.path.join(MAINROOT, 'inputs\\'+ input_filename)
+    DEFAULT_OUTPUT_FILE = os.path.join(MAINROOT, 'outputs\\'+ output_filename)
     
 ACCEPTED_EXTENSIONS = ('csv', 'xlsx', 'xls')
 DEFAULT_EXT = 'xlsx'
