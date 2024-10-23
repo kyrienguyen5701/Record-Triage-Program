aeba1f88558e153fb5db6ddc32452ae10e16511c
libstudent <libstudent@union.edu>
2024-10-23 15:38:47 -0400

    added check for wether .env file already exists

--- scripts/setup.cmd
+++ scripts/setup.cmd
@@ -4,24 +4,37 @@ set app_dir=%root_dir%\app
 SET folder=%~dp0..\
 
 @echo off
+IF EXIST %app_dir%\src\.env (
+    echo API Key located ...
+) ELSE (
+set /p APIKey="Enter your Alma API key: "
+echo ALMA_API_KEY=%APIKey% > %app_dir%\src\.env && IF EXIST %app_dir%\src\sample.env (@DEL %app_dir%\src\sample.env)
+echo Writing API key ...
+echo:
+)
+
+@echo off
+IF EXIST %userprofile%\Desktop\Triage Program.lnk (
+    echo Desktop Shortcut Located ...
+    :: add empty line to make output more readable
+    echo:
+) ELSE (
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
 echo sLinkFile = "%userprofile%\Desktop\Triage Program.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 echo oLink.TargetPath = "%folder%\app\main.cmd%" >> CreateShortcut.vbs
 echo oLink.Description = "Triage V2.1" >> CreateShortcut.vbs
-echo oLink.IconLocation = "%folder%\app\assets\icon.ico" >> CreateShortcut.vbs
+echo oLink.IconLocation = "%folder%\app\src\assets\icon.ico" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
 cscript CreateShortcut.vbs
 del CreateShortcut.vbs
-
-set /p APIKey="Enter your Alma API key: "
-@echo off
-echo ALMA_API_KEY=%APIKey% > %app_dir%\src\.env && IF EXIST %app_dir%\src\sample.env (@DEL %app_dir%\src\sample.env)
-echo Writing API key ...
+)
 
 echo Installing requirements ...
 pip install -r %root_dir%\requirements.txt
 
+:: add empty line to make output more readable
+echo:
 echo Creating logging directory ...
 mkdir %app_dir%\logs
 
